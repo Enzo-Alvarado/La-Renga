0e90a11291c358565bc8a32ed70bb81a2d8fb23a
Enzo Alvarado <alvaradoenzo4@gmail.com>
2024-10-01 15:55:18 -0300

    Commit con el proyecto completo

---
+++ album.php
@@ -0,0 +1,271 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!--CSS-->
+    <link rel="stylesheet" href="style.css">
+    <title>Album y Discos</title>
+</head>
+<body>
+<header>
+      <!--    BARRA DE NAVEGACIÓN     -->
+      <nav class="contenedor-navbar">
+        <ul class="lista-nav">
+          <li class="item-nav"><a href="index.php">Inicio</a></li>
+        </ul>
+      </nav>
+    </header>
+
+    <main>
+        <section class="contenedor-album">
+            <div class="contenedor-titulo-album">
+                <h3>Albums y Discos</h3>
+            </div>
+            <div class="contenedor-tabla">
+            <table class="tabla-musica">
+            <tr class="registro-tabla">
+              <td class="campo-tabla">
+                <p class="nombre-campo">Disco/Album</p>
+              </td>
+              <td class="campo-tabla">
+                <p class="nombre-campo">Fecha de lanzamiento</p>
+              </td>
+              <td class="campo-tabla">
+                <p class="nombre-campo">Genero</p>
+              </td>
+              <td class="campo-tabla">
+                <p class="nombre-campo">Canciones</p>
+              </td>
+            </tr>
+            <tr class="registro-tabla">
+              <td class="campo-tabla">
+                <p>Esquivando charcos</p>
+              </td>
+              <td class="campo-tabla">
+                <p>1991</p>
+              </td>
+              <td class="campo-tabla">
+                <p>Rock, Hard Rock</p>
+              </td>
+              <td class="campo-tabla">
+                <ul>
+                  <li>La nave del olvido</li>
+                  <li>Moscas verdes para el charlatán</li>
+                  <li>Embrolos, fatos y paquetes</li>
+                  <li>Somos los mismos de siempre</li>
+                  <li>El viento que todo empuja</li>
+                  <li>Ruta 40</li>
+                  <li>Blues de Bolivia</li>
+                  <li>Voy a bailar a la nave del olvido</li>
+                  <li>Embrollos, fatos y paquetes (versión extendida)</li>
+                </ul>
+              </td>
+              <tr class="registro-tabla">
+              <td class="campo-tabla">
+                <p>Adónde me lleva la vida</p>
+              </td>
+              <td class="campo-tabla">
+                <p>1994</p>
+              </td>
+              <td class="campo-tabla">
+                <p>Rock, Blues Rock</p>
+              </td>
+              <td class="campo-tabla">
+                <ul>
+                  <li>El final es en donde partí</li>
+                  <li>El hombre de la estrella</li>
+                  <li>Moscas verdes para el charlatán</li>
+                  <li>Sombras luminosas</li>
+                  <li>Adonde me lleva la vida</li>
+                  <li>El viento que todo empuja</li>
+                  <li>Voy a bailar a la nave del olvido</li>
+                  <li>A la carga, mi rocanrol</li>
+                </ul>
+              </td>
+              <tr class="registro-tabla">
+              <td class="campo-tabla">
+                <p>Despedazado por mil partes</p>
+              </td>
+              <td class="campo-tabla">
+                <p>1996</p>
+              </td>
+              <td class="campo-tabla">
+                <p>Hard Rock</p>
+              </td>
+              <td class="campo-tabla">
+                <ul>
+                  <li>Desnudo para siempre (o Despedazado por mil partes)</li>
+                  <li>Veneno</li>
+                  <li>A la carga, mi rocanrol</li>
+                  <li>Paja brava</li>
+                  <li>El final es en donde partí</li>
+                  <li>Balada del diablo y la muerte</li>
+                  <li>La razón que te demora</li>
+                  <li>Psilocybe Mexicana</li>
+                  <li>Lo frágil de la locura</li>
+                  <li>El rey de la triste felicidad</li>
+                  <li>Hablando de la libertad</li>
+                </ul>
+              </td>
+              <tr class="registro-tabla">
+              <td class="campo-tabla">
+                <p>La Razon Que Te Demora</p>
+              </td>
+              <td class="campo-tabla">
+                <p>1998</p>
+              </td>
+              <td class="campo-tabla">
+                <p>Rock</p>
+              </td>
+              <td class="campo-tabla">
+                <ul>
+                  <li>El final es en donde partí</li>
+                  <li>El rey de la triste felicidad</li>
+                  <li>Hablando de la libertad</li>
+                  <li>Psilocybe Mexicana</li>
+                  <li>Desnudo para siempre (Despedazado por mil partes)</li>
+                  <li>Lo frágil de la locura</li>
+                  <li>Paja brava</li>
+                  <li>Veneno</li>
+                  <li>Balada del diablo y la muerte</li>
+                  <li>A la carga, mi rocanrol</li>
+                </ul>
+              </td>
+              <tr class="registro-tabla">
+              <td class="campo-tabla">
+                <p>La esquina del infinito</p>
+              </td>
+              <td class="campo-tabla">
+                <p>2000</p>
+              </td>
+              <td class="campo-tabla">
+                <p>Hard Rock, Blues Rock</p>
+              </td>
+              <td class="campo-tabla">
+                <ul>
+                  <li>El final es en donde partí</li>
+                  <li>Panic show</li>
+                  <li>El rey de la triste felicidad</li>
+                  <li>El cielo del desengaño</li>
+                  <li>El ojo del huracán</li>
+                  <li>La balada del diablo y la muerte</li>
+                  <li>Hablando de la libertad</li>
+                  <li>El hombre de la estrella</li>
+                  <li>Blues de Bolivia</li>
+                  <li>El viento que todo empuja</li>
+                </ul>
+              </td>
+            </tr>
+            <tr class="registro-tabla">
+              <td class="campo-tabla">
+                <p>Detonador de sueños</p>
+              </td>
+              <td class="campo-tabla">
+                <p>2003</p>
+              </td>
+              <td class="campo-tabla">
+                <p>Hard Rock</p>
+              </td>
+              <td class="campo-tabla">
+                <ul>
+                  <li>A tu lado</li>
+                  <li>Detonador de sueños</li>
+                  <li>El ojo del huracán</li>
+                  <li>La razón que te demora</li>
+                  <li>Panic show</li>
+                  <li>Blues de Bolivia</li>
+                  <li>Hablando de la libertad</li>
+                  <li>La nave del olvido</li>
+                  <li>Veneno</li>
+                  <li>La vida en bicicleta</li>
+                </ul>
+              </td>
+            </tr>
+            <tr class="registro-tabla">
+              <td class="campo-tabla">
+                <p>Trueno Tierra</p>
+              </td>
+              <td class="campo-tabla">
+                <p>2006</p>
+              </td>
+              <td class="campo-tabla">
+                <p>Rock, Hard Rock</p>
+              </td>
+              <td class="campo-tabla">
+                <ul>
+                  <li>El ojo del huracán</li>
+                  <li>La nave del olvido</li>
+                  <li>Blues de Bolivia</li>
+                  <li>Panic show</li>
+                  <li>El cielo del desengaño</li>
+                  <li>Hablando de la libertad</li>
+                  <li>Detonador de sueños</li>
+                  <li>A tu lado</li>
+                  <li>Veneno</li>
+                  <li>La balada del diablo y la muerte</li>
+                </ul>
+              </td>
+            </tr>
+            <tr class="registro-tabla">
+              <td class="campo-tabla">
+                <p>Algún Rayo</p>
+              </td>
+              <td class="campo-tabla">
+                <p>2010</p>
+              </td>
+              <td class="campo-tabla">
+                <p>Rock</p>
+              </td>
+              <td class="campo-tabla">
+                <ul>
+                  <li>Canibalismo galáctico</li>
+                  <li>Oscuro diamante</li>
+                  <li>La furia de la bestia rock</li>
+                  <li>Algún rayo</li>
+                  <li>Dioses de terciopelo</li>
+                  <li>Ruta 40</li>
+                  <li>Blues de Bolivia</li>
+                  <li>Detonador de sueños</li>
+                  <li>El viento que todo empuja</li>
+                  <li>La razón que te demora</li>
+                </ul>
+              </td>
+            </tr>
+            <tr class="registro-tabla">
+              <td class="campo-tabla">
+                <p>Pesados Vestigios</p>
+              </td>
+              <td class="campo-tabla">
+                <p>2014</p>
+              </td>
+              <td class="campo-tabla">
+                <p>Hard Rock</p>
+              </td>
+              <td class="campo-tabla">
+                <ul>
+                  <li>Corazón fugitivo</li>
+                  <li>Mirada de acantilado</li>
+                  <li>Sabes que</li>
+                  <li>Oscuro diamante</li>
+                  <li>Blues de Bolivia</li>
+                  <li>Panic show</li>
+                  <li>Detonador de sueños</li>
+                  <li>Hablando de la libertad</li>
+                  <li>La balada del diablo y la muerte</li>
+                </ul>
+              </td>
+            </tr>
+          </table>
+            </div>
+        </section>
+    </main>
+
+    <!--    FOOTER O PIE DE PAGINA     -->
+    <footer>
+      <p>Desarrollado por Enzo Alvarado</p>
+      <p>©Derechos reservados</p>
+    </footer>
+</body>
+</html>
---
+++ biografia.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+    <!--CSS-->
+    <link rel="stylesheet" href="style.css" />
+    <title>Biografia Individual</title>
+  </head>
+  <body>
+    <header>
+      <!--    BARRA DE NAVEGACIÓN     -->
+      <nav class="contenedor-navbar">
+        <ul class="lista-nav">
+          <li class="item-nav"><a href="index.php">Inicio</a></li>
+        </ul>
+      </nav>
+    </header>
+
+    <!--  CUERPO DE LA PAGINA    -->
+    <main class="contenedor-main">
+      <section class="contenedor-biografia">
+        <div class="contenedor-titulo-biografia">
+          <h2>Biografia Individual</h2>
+        </div>
+        <div class="contenedor-artista">
+          <img src="image/artista1.webp" alt="" />
+          <div>
+            <h3>Gustavo "Chizzo" Nápoli:</h3>
+            <ul>
+              <li>Rol: Voz y guitarra líder.</li>
+              <li>Nacimiento: 1 de agosto de 1966.</li>
+              <li>Historia: Es el principal compositor de la banda y el alma creativa detrás de La Renga. Su estilo vocal distintivo y sus letras cargadas de poesía urbana han sido clave para el éxito del grupo. Además de tocar la guitarra, es el encargado de escribir la mayoría de las canciones que definen el sonido de la banda.</li>
+            </ul>
+          </div>
+        </div>
+        <div class="contenedor-artista">
+          <div>
+            <h3>Gabriel "Tete" Iglesias:</h3>
+            <ul>
+              <li>Rol: Bajista.</li>
+              <li>Nacimiento: 8 de marzo de 1967.</li>
+              <li>Historia: Es uno de los miembros fundadores de la banda. A lo largo de los años, su estilo sólido y contundente en el bajo ha sido fundamental para el característico sonido de La Renga. Junto con su hermano Jorge, ha formado la base rítmica que sostiene la potencia del grupo.</li>
+            </ul>
+          </div>
+          <img src="image//artista2.jpg" alt="" />
+        </div>
+        <div class="contenedor-artista">
+          <img src="image/artista3.jpg" alt="" />
+          <div>
+            <h3>Jorge "Tanque" Iglesias:</h3>
+            <ul>
+              <li>Rol: Baterista.</li>
+              <li>Nacimiento: 26 de febrero de 1968.</li>
+              <li>Historia: Es el baterista de la banda y hermano de Tete. Su estilo poderoso y preciso ha contribuido al sonido inconfundible de La Renga, con ritmos que complementan las guitarras estridentes de Chizzo y la fuerza del bajo de Tete. También es un miembro fundador de la banda.</li>
+            </ul>
+          </div>
+        </div>
+        <div class="contenedor-artista">
+          <div>
+            <h3>Manuel "Manu" Varela:</h3>
+            <ul>
+              <li>Rol: Teclados, armónica y saxofón.</li>
+              <li>Nacimiento: 27 de agosto de 1979.</li>
+              <li>Historia: Se unió a la banda en la década de los 2000 para darle un nuevo matiz a su sonido. Varela aporta diversidad musical a través de su habilidad con múltiples instrumentos, incluyendo el saxofón, que ha enriquecido el sonido más rockero de La Renga con tintes de blues y otros géneros.</li>
+            </ul>
+          </div>
+          <img src="image/artista4.jpg" alt="" />
+        </div>
+        <div class="contenedor-artista">
+          <img src="image/artista5.jpg" alt="" />
+          <div>
+            <h3>Raúl "Locura" Dilelio:</h3>
+            <ul>
+              <li>Rol: Guitarrista (ex miembro).</li>
+              <li>Historia: Fue uno de los miembros fundadores de La Renga, pero dejó la banda tras la grabación del primer disco. Su salida marcó una transición en el sonido del grupo hacia una dirección más potente y menos ligada al hard rock clásico, pero su influencia en los primeros años fue significativa.</li>
+            </ul>
+          </div>
+        </div>
+      </section>
+    </main>
+
+    <!--    FOOTER O PIE DE PAGINA     -->
+    <footer>
+      <p>Desarrollado por Enzo Alvarado</p>
+      <p>©Derechos reservados</p>
+    </footer>
+  </body>
+</html>
